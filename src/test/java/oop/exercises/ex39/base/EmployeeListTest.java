e86a8e16111564cfd83719e1e22822ca5a523be0
xera51 <xera51@protonmail.com>
2021-06-13 22:25:47 -0400

    Solution 40 Final

--- src/test/java/oop/exercises/ex39/base/EmployeeListTest.java
+++ src/test/java/oop/exercises/ex39/base/EmployeeListTest.java
@@ -4,9 +4,48 @@
  */
 package oop.exercises.ex39.base;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeListTest {
-    // really not sure what to be testing on the second half of these problems.
-    // most public methods have been void
+
+    @Test
+    @DisplayName("Ex39: Table is generated correctly")
+    void table_of_all_employees_is_generated_correctly_set1() {
+        EmployeeList list = new EmployeeList();
+        Main app = new Main();
+
+        app.employeeEntries(list);
+
+        String expected = String.format("Name                | Position          | Separation Date%n" +
+                "--------------------|-------------------|----------------%n" +
+                "Jacquelyn Jackson   | DBA               |                %n" +
+                "Jake Jacobson       | Programmer        |                %n" +
+                "John Johnson        | Manager           | 2016-12-31     %n" +
+                "Michaela Michaelson | District Manager  | 2015-12-19     %n" +
+                "Sally Weber         | Web Developer     | 2015-12-18     %n" +
+                "Tou Xiong           | Software Engineer | 2016-10-05     %n");
+        String actual = list.displayEmployeesByLastName();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Ex40: Table is generated correctly")
+    void table_of_all_employees_is_generated_correctly_set2() {
+        EmployeeList list = new EmployeeList();
+        Main app = new Main();
+
+        app.employeeEntries(list);
+
+        String expected = String.format("Name                | Position          | Separation Date%n" +
+                "--------------------|-------------------|----------------%n" +
+                "Jacquelyn Jackson   | DBA               |                %n" +
+                "Jake Jacobson       | Programmer        |                %n");
+        String actual = list.displayFilteredTable("Jac");
+
+        assertEquals(expected, actual);
+    }
 }
